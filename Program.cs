b2a0f8e5ae6bef7034843d318886564142e24769
Kelly Duong <kellyduong@usf.edu>
2020-01-31 18:54:30 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace UserInputKellyDuong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, this is Kelly Duong");
+            Console.WriteLine("Type good evening and hit enter!");
+            string greeting;
+            greeting = Console.ReadLine();
+            Console.WriteLine("You typed: " + greeting);
+        }
+    }
+}
